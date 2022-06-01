8aeae676b380b36275362b746b3ae3c3b20670e5
ClemRoy <clement.roy1@gmail.com>
2022-06-01 02:54:24 +0200

    User system and inscription

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractBaseUser):
+    username = models.CharField(max_length=25, unique=True)
+
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = [username]
